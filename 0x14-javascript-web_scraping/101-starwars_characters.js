9ff5e83827ed7113debd49d322641e8ce4fe187d
root <you@example.com>
2024-02-20 04:10:58 -0800

    101 starwars

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,27 @@
 #!/usr/bin/node
-
+// get request
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const movieId = process.argv[2];
+if (!movieId) process.exit();
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
+const filmsURL = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+function makeRequest (url, charactersInMovie, index) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < charactersInMovie.length) {
+        makeRequest(charactersInMovie[index + 1], charactersInMovie, index + 1);
+      }
     }
-  }
+  });
+}
+
+request.get(filmsURL, (error, response, body) => {
+  if (!error) {
+    const charactersInMovie = JSON.parse(body).characters;
+    makeRequest(charactersInMovie[0], charactersInMovie, 0);
+  } else console.log(error);
 });
